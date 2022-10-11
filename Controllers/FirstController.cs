7a8c0f65911628be35f52bbeadb5f8cc56169313
oana oprea <oanamaria.oprea@yahoo.com>
2022-10-11 19:28:31 +0300

    lab 1 gata

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Oprea_Oana_Lab1.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Welcome, Oana!");
+        }
+
+        public IActionResult Salut()
+        {
+            return Content("Buna seara, Oana!");
+        }
+
+        public IActionResult Varsta(string nume, int varsta)
+        {
+            return Content(nume+" are varsta de "+varsta+" ani.");
+        }
+    }
+}
